0520ec9cd0d2f7206b1fde93f996983a71c4744f
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-11-07 15:39:23 -0500

    added generic console input and output methods

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,39 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static String getConsoleInput(String prompt)
+    /**
+     * This is a generic Console Input Function that gets input from the keyboard
+     * and saves the data to an element of an array.
+     *
+     * @param prompt {String}
+     * @param object {T[]}
+     * @return
+     * @param <T>
+     */
+    public static <T> T getConsoleInputToArray(String prompt, T[] object)
     {
         Scanner console = new Scanner(System.in);
         System.out.print(prompt);
 
-        return console.nextLine();
+        switch (object.getClass().getSimpleName())
+        {
+            case "Double[]":
+                return (T) ((Double)console.nextDouble());
+            case "Integer[]":
+                return (T) ((Integer)console.nextInt());
+            case "Float[]":
+                return (T) ((Float)console.nextFloat());
+            default:
+                return (T) console.nextLine();
+        }
     }
 
-    public static void displayArray(String[] array)
+    /**
+     * This Method displays all the elements of an Array
+     * @param array
+     * @param <T>
+     */
+    public static <T> void displayArray(T[] array)
     {
         for (var element: array)
         {
@@ -22,12 +46,23 @@ public class Main {
     public static void main(String[] args)
     {
         String[] myStrings = new String[Config.NUM_OF_STRINGS]; // new empty container of type String
+        Integer[] myInts = new Integer[Config.NUM_OF_INTEGERS];
+
 
         for (int i = 0; i < Config.NUM_OF_STRINGS; i++)
         {
-            myStrings[i] = getConsoleInput("Please enter your name: ");
+            myStrings[i] = getConsoleInputToArray("Please enter your name: ", myStrings);
         }
 
         displayArray(myStrings);
+
+        for (int i = 0; i < Config.NUM_OF_INTEGERS; i++)
+        {
+            myInts[i] = getConsoleInputToArray("Please enter your number: ", myInts);
+        }
+
+        displayArray(myInts);
+
+
     }
 }
